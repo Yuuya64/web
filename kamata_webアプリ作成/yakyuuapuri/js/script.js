1331a0f19ef52ce9b88cea6bb797af21b4c454ca
E4\E4 <jt-202509@kurihara.ac.jp>
2026-05-14 12:21:33 +0900

    コミットしました

---
+++ kamata_webアプリ作成/yakyuuapuri/js/script.js
@@ -0,0 +1,31 @@
+// const loadingAreaGrey = document.querySelector('#loading');
+// const loadingAreaGreen = document.querySelector('#loadingscreen');
+
+// window.addEventListener('load', () => {
+//     // グレーのエリア：フェードアウト
+//     loadingAreaGrey.animate(
+//         {
+//             opacity: [1, 0],
+//             visibility: 'hidden',
+//         },
+//         {
+//             duration: 2000,
+//             delay: 1200,
+//             easing: 'ease',
+//             fill: 'forwards',
+//         }
+//     );
+
+//     // グリーンのエリア：下から上へスライド
+//     loadingAreaGreen.animate(
+//         {
+//             translate: ['0 100vh', '0 0', '0 -100vh']
+//         },
+//         {
+//             duration: 2000,
+//             delay: 800,
+//             easing: 'ease',
+//             fill: 'forwards',
+//         }
+//     );
+// });
